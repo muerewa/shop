37b4efdefe720c5cd255b1ee63cd3f856daea775
muerewa <muerewaa@gmail.com>
2022-04-01 03:54:13 +0000

    added admin panel

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,34 +1,66 @@
-import React, { useContext } from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
-import {NavLink} from 'react-router-dom'
-import { Context } from '../index'
-import { MAIN_ROUTE } from '../utils/consts'
-import { Button } from 'react-bootstrap'
-import { observer } from 'mobx-react-lite'
+import React, { useContext } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import { NavLink } from "react-router-dom";
+import { Context } from "../index";
+import {
+  ADMIN_ROUTE,
+  LOGIN_ROUTE,
+  MAIN_ROUTE,
+  REG_ROUTE,
+} from "../utils/consts";
+import { Button } from "react-bootstrap";
+import { observer } from "mobx-react-lite";
+import { useHistory } from "react-router-dom";
 
-const NavBar =observer(() => {
-    const {user} = useContext(Context) 
+const NavBar = observer(() => {
+  const { user } = useContext(Context);
+  const history = useHistory();
   return (
     <Navbar bg="dark" variant="dark">
-        <Container>
-                <NavLink to={MAIN_ROUTE} style={{color: 'white'}}>Главная</NavLink>
-            {user.isAuth ? 
-                <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={"outline-light"}>Корзина</Button>
-                        <Button variant={"outline-light"} className="ms-4">Выйти</Button>
-                        <Button variant={"outline-light"} className="ms-4">Админ панель</Button>
-                </Nav>
-            : 
-                <Nav className="ml-auto" style={{color: 'white'}}>
-                    <Button variant={"outline-light"}>Авторизация</Button>
-                    <Button variant={"outline-light"}  className="ms-4">Регистрация</Button>
-                </Nav>
-            }
-        </Container>
-  </Navbar>
-  )
-})
+      <Container>
+        <NavLink to={MAIN_ROUTE} style={{ color: "white" }}>
+          Главная
+        </NavLink>
+        {user.isAuth ? (
+          <Nav className="ml-auto" style={{ color: "white" }}>
+            <Button variant={"outline-light"}>Корзина</Button>
+            <Button
+              variant={"outline-light"}
+              className="ms-4"
+              onClick={() => history.push(LOGIN_ROUTE)}
+            >
+              Выйти
+            </Button>
+            <Button
+              variant={"outline-light"}
+              className="ms-4"
+              onClick={() => history.push(ADMIN_ROUTE)}
+            >
+              Админ панель
+            </Button>
+          </Nav>
+        ) : (
+          <Nav className="ml-auto" style={{ color: "white" }}>
+            <Button
+              variant={"outline-light"}
+              onClick={() => history.push(LOGIN_ROUTE)}
+            >
+              Авторизация
+            </Button>
+            <Button
+              variant={"outline-light"}
+              onClick={() => history.push(REG_ROUTE)}
+              className="ms-4"
+            >
+              Регистрация
+            </Button>
+          </Nav>
+        )}
+      </Container>
+    </Navbar>
+  );
+});
 
-export default NavBar
+export default NavBar;
--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -1,26 +1,25 @@
-const {makeAutoObservable} = require('mobx')
+const { makeAutoObservable } = require("mobx");
 
 export default class UserStore {
-    constructor() {
-        this._isAuth = false
-        this._user = {}
-        makeAutoObservable(this) 
-    }
+  constructor() {
+    this._isAuth = true;
+    this._user = {};
+    makeAutoObservable(this);
+  }
 
-    setIsAuth (bool) {
-        this._isAuth = bool
-    }
+  setIsAuth(bool) {
+    this._isAuth = bool;
+  }
 
-    setUser (user) {
-        this._user = user
-    }
+  setUser(user) {
+    this._user = user;
+  }
 
-    get isAuth() {
-        return this._isAuth
-    }
+  get isAuth() {
+    return this._isAuth;
+  }
 
-    get user() {
-        return this._user
-    }
-
+  get user() {
+    return this._user;
+  }
 }
