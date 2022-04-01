ac1a63e32738d0ec98550b905e9ecea0beca6ab7
muerewa <muerewaa@gmail.com>
2022-04-01 01:36:50 +0000

    added auth/reg forms

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import Row from 'react-bootstrap/Row'
 import Card from "react-bootstrap/Card";
+import { REG_ROUTE, LOGIN_ROUTE } from "../utils/consts";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Auth = () => {
+  const location = useLocation()
+  const isLogin = location.pathname === LOGIN_ROUTE
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
       style={{ height: window.innerHeight - 54 }}
     >
       <Card style={{ width: 600 }} className="p-5">
-        <h2 className="m-auto">Авторизация</h2>
+        <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
         <Form className="d-flex flex-column">
           <Form.Control
             className="mt-4"
@@ -19,7 +24,11 @@ const Auth = () => {
             className="mt-4"
             placeholder="Введите пароль..."
           />
-          <Button variant="outline-success" className="mt-3 align-self-end">Войти</Button>
+          <Row className="d-flex justify-content-between mt-3">
+              <NavLink to={isLogin ? REG_ROUTE : LOGIN_ROUTE} style={{width: 200}}>{isLogin ? "Еще нет аккаунта?" : 'Уже есть аккаунт?'}</NavLink>
+            <Button  variant={"outline-success"}>{isLogin ? 'Войти' : 'Зарегистрироваться'}</Button>
+          </Row>
+          
         </Form>
       </Card>
     </Container>
